60ae90a308f3624373610649d8b4236ccfc78cac
Aidan Woods <aidantwoods@gmail.com>
2016-07-25 22:33:53 +0100

    command line options

--- jsChar2Code.py
+++ jsChar2Code.py
@@ -1,6 +1,30 @@
-a = input("")
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="File to read from")
+parser.add_argument("-o", "--output", help="If set, output will be supressed from console and written to file name specified")
+
+args = parser.parse_args()
+
+if args.file:
+	i = open(args.file, 'r')
+	a = i.read()
+	i.close()
+else:
+	a = input("")
+
 s = []
 for l in a:
 	s.append(str(ord(l)))
 
-print("String.fromCharCode("+",".join(s)+")")
+while s[-1] == '10':
+	s.pop()
+
+out = "String.fromCharCode("+",".join(s)+")"
+
+if args.output:
+	o = open(args.output, 'w')
+	o.write(out)
+	o.close()
+else:
+	print(out)
